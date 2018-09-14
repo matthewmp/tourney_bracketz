51f8db772ad19eb0ec7188cba42a36a47551708a
Tom Cariello <tomcariello@gmail.com>
2018-09-14 07:58:22 -0400

    began working on the login / sign up modal

--- src/client/js/signin.js
+++ src/client/js/signin.js
@@ -1,1 +1,32 @@
-console.log("testing signin.js");
+// Listen for clicks on the sign in button in the navbar
+document.getElementById("signinbutton").addEventListener("click", function() {
+    //show the modal
+    document.getElementById("signincontainer").style.display = "block"
+});
+
+// Listen for clicks on the log in close button in the sign in modal
+document.getElementById("loginclosebutton").addEventListener("click", function() {
+    // Close the modal
+    document.getElementById("signincontainer").style.display = "none"
+});
+
+// Listen for clicks on the log in button in the sign in modal
+document.getElementById("loginbutton").addEventListener("click", function() {
+    console.log('log in button clicked');
+
+    // Check if #username has been entered
+    if ( document.getElementById("username").value == "" ) {
+        // Alert user
+        document.getElementById("usernamelabel").style.color = "red"
+    } else {
+        document.getElementById("usernamelabel").style.color = ""
+    }
+    // Check if #password has been entered
+    if ( document.getElementById("password").value == "" ) {
+        // Alert user
+        document.getElementById("passwordlabel").style.color = "red"
+    } else {
+        document.getElementById("passwordlabel").style.color = ""
+    }
+
+});
