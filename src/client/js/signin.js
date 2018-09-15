7c494f242735f999bd6f244a4a94a48f012c5483
Tom Cariello <tomcariello@gmail.com>
2018-09-15 15:34:15 -0400

    removed js validation on login forms in favor of 'required' in the input. Also fixed signin.js to load after the DOM

--- src/client/js/signin.js
+++ src/client/js/signin.js
@@ -1,32 +1,19 @@
-// Listen for clicks on the sign in button in the navbar
-document.getElementById("signinbutton").addEventListener("click", function() {
-    //show the modal
-    document.getElementById("signincontainer").style.display = "block"
-});
+window.onload = () => {
 
-// Listen for clicks on the log in close button in the sign in modal
-document.getElementById("loginclosebutton").addEventListener("click", function() {
-    // Close the modal
-    document.getElementById("signincontainer").style.display = "none"
-});
+    // Listen for clicks on the sign in button in the navbar
+    document.getElementById("signinbutton").addEventListener("click", function() {
+        //show the modal
+        document.getElementById("signincontainer").style.display = "block"
+    });
 
-// Listen for clicks on the log in button in the sign in modal
-document.getElementById("loginbutton").addEventListener("click", function() {
-    console.log('log in button clicked');
+    // Listen for clicks on the log in close button in the sign in modal
+    document.getElementById("loginclosebutton").addEventListener("click", function() {
+        // Close the modal
+        document.getElementById("signincontainer").style.display = "none"
+    });
 
-    // Check if #username has been entered
-    if ( document.getElementById("username").value == "" ) {
-        // Alert user
-        document.getElementById("usernamelabel").style.color = "red"
-    } else {
-        document.getElementById("usernamelabel").style.color = ""
-    }
-    // Check if #password has been entered
-    if ( document.getElementById("password").value == "" ) {
-        // Alert user
-        document.getElementById("passwordlabel").style.color = "red"
-    } else {
-        document.getElementById("passwordlabel").style.color = ""
-    }
-
-});
+    // Listen for clicks on the log in button in the sign in modal
+    document.getElementById("loginbutton").addEventListener("click", function() {
+        console.log('log in button clicked');
+    });
+}
