c56999588a2981406553d5f398acc54cb8c446ef
Tom Cariello <tomcariello@gmail.com>
2018-12-31 10:31:25 -0500

    adjusted DB query on the landing page to properly parse the login status. This was required due to the dashboard fixes.

--- src/server/routes.js
+++ src/server/routes.js
@@ -60,12 +60,25 @@ module.exports = function(app, passport,models) {
         let data = [{User: false}];
         
         if ( confirmUserSession(req) == true ) {
-            data = [{User: true}]
-        }
+            // Find the User
+            models.User.findOne({
+                where: {
+                    id: req.user.id
+                }
+            }).then(function(data) {
+                // Package the returned JSON file
+                var payload = {tournamentdata: data};
 
-        // The first argument is the file to load. In this case, index.pug
-        // Second argument is the data payload to be rendered
-        res.render('index', {tournamentdata: data});
+                // Parse the payload data within userdashboard.pug before sending the result to the client
+                res.render('index', {tournamentdata: payload.tournamentdata});
+            }).catch(function (err) {
+                console.log(err);
+            });
+        } else {
+            // The first argument is the file to load. In this case, index.pug
+            // Second argument is the data payload to be rendered
+            res.render('index', {tournamentdata: data});
+        }
     })
 
     // Access the specific user dashboard. Only accessible when logged in.
@@ -198,7 +211,6 @@ module.exports = function(app, passport,models) {
     app.get('/deletetournament/:tournamentID', isLoggedIn, (req,res) => {
         
         // This should be done in one command; research & fix to remove the second execution
-
 
         // Delete the *Tournament*
         models.Tournament.destroy({
